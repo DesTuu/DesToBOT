431e66f4336e4d7eb4eda6f4967c3d5df2dd277c
DesTu <maurycy-vip@wp.pl>
2025-01-27 15:24:11 +0100

    pattern_update

--- cogs/regex_url_checker.py
+++ cogs/regex_url_checker.py
@@ -22,7 +22,7 @@ class RegexUrlChecker(commands.Cog):
                 if url_regex:
                     # print("Wysłano link")
                     acceptable_pattern = (r"https?://(www\.)?"
-                                          r"(youtube\.com|instagram\.com|facebook\.com|tiktok\.com|cdn.discordapp\.com|tenor\.com)")
+                                          r"(youtube\.com|instagram\.com|facebook\.com|tiktok\.com|[a-z]*\.?discordapp\.(net|com)|tenor\.com)")
                     acceptable_regex = re.search(acceptable_pattern, message.content)
 
                     if not acceptable_regex:
