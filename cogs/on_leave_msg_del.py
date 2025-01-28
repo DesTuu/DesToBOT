b31d2c1f31c691968c45557d51c86088a82d9525
DesTu <maurycy-vip@wp.pl>
2025-01-28 22:17:22 +0100

    on_member_remove2

--- cogs/on_leave_msg_del.py
+++ cogs/on_leave_msg_del.py
@@ -10,7 +10,7 @@ class OnLeaveMsgDel(commands.Cog):
     async def on_member_remove(self, member: discord.Member):
         if member.guild.id == 1056134342826528808:
             general_channel = self.bot.get_channel(1056276694140452964)
-            async for message in general_channel.history(limit=50):
+            async for message in general_channel.history(limit=100):
                 if str(message.author.id).strip() == "282859044593598464":
                     if f"<@!{member.id}>" in message.content:
                         await message.delete()
