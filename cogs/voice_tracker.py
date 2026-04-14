b882c7881b3faf6971242220dc757c9b9e037419
DesTu <maurycy-vip@wp.pl>
2026-04-14 13:01:51 +0200

    voice_tracker_50days

--- cogs/voice_tracker.py
+++ cogs/voice_tracker.py
@@ -21,9 +21,9 @@ class VoiceTracker(commands.Cog):
             return []
 
         messages = []
-        limit_date = datetime.now(timezone.utc) - timedelta(days=40)
+        limit_date = datetime.now(timezone.utc) - timedelta(days=50)
 
-        async for message in channel.history(limit=10000):
+        async for message in channel.history(limit=20000):
             if message.created_at < limit_date:
                 break
             messages.append(message)
